d86fa25690db7af1da6bd97a9dd157ea28b2117b
dzakwanIrfan <lexdani368@gamil.com>
2023-10-25 07:38:06 +0700

    menambah fitur update

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -67,7 +67,10 @@ class DashboardPostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('dashboard.post.edit', [
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
     }
 
     /**
@@ -75,10 +78,24 @@ class DashboardPostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        return view('dashboard.post.edit', [
-            'post' => $post,
-            'categories' => Category::all()
-        ]);
+        $rules = [
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required'
+        ];
+
+        if($request->slug != $post->slug){
+            $rules['slug'] = 'required|unique:post';
+        }
+
+        $validatedData = $request->validate($rules);
+
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 150);
+
+        Post::where('id', $post->id)->update($validatedData);
+
+        return redirect('dashboard/posts')->with('success','Post has been updated!');
     }
 
     /**
--- resources/views/dashboard/post/edit.blade.php
+++ resources/views/dashboard/post/edit.blade.php
@@ -11,7 +11,7 @@
             @csrf
             <div class="mb-3">
               <label for="title" class="form-label">Title</label>
-              <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" required autofocus value="{{ old('title') }}">
+              <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" required autofocus value="{{ old('title', $post->title) }}">
               @error('title')
                 <div class="invalid-feedback">
                   {{ $message }}
@@ -20,7 +20,7 @@
             </div>
             <div class="mb-3">
               <label for="slug" class="form-label">Slug</label>
-              <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" value="{{ old('slug') }}">
+              <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" value="{{ old('slug', $post->slug) }}">
               @error('slug')
                 <div class="invalid-feedback">
                   {{ $message }}
@@ -31,7 +31,7 @@
               <label for="category" class="form-label">Category</label>
               <select class="form-select" name="category_id">
                 @foreach ($categories as $category)
-                  @if (old('category_id') == $category->id)
+                  @if (old('category_id', $post->category_id) == $category->id)
                     <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
                   @else
                     <option value="{{ $category->id }}">{{ $category->name }}</option>
@@ -46,11 +46,11 @@
                   {{ $message }}
                 </div>
               @enderror
-              <input id="body" type="hidden" name="body" value="{{ old('body') }}">
+              <input id="body" type="hidden" name="body" value="{{ old('body', $post->body) }}">
 
               <trix-editor input="body"></trix-editor>
             </div>
-            <button type="submit" class="btn btn-primary">Create post</button>
+            <button type="submit" class="btn btn-primary">Update post</button>
         </form>
     </div>
 
